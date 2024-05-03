cbee7f9b0353c862eb396020290c0826f9a3519c
Argen Egeshov <argfan2@gmail.com>
2024-05-04 01:04:05 +0600

    04.05.24

--- src/components/SvgContainer.vue
+++ src/components/SvgContainer.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { useMouseInElement } from '@vueuse/core';
-import { reactive, ref } from 'vue';
+import { reactive, ref, computed } from 'vue';
 import SvgRect from './SvgRect.vue'
 import RRect from '../models/RRect';
 
@@ -8,48 +8,61 @@ const target = ref(null)
 
 const { x, y, elementX, elementY, isOutside } = useMouseInElement(target)
 
-const rect = reactive(new RRect())
+// const rect = reactive(new RRect())
+const rect = reactive({
+  x: 0, y: 0, x1: 0, y1: 0, x2: 0, y2: 0
+})
 
 const isDraw = ref(false)
 
-const mouseDown = ()=>{
-  isDraw.value=true
-  rect.x1 = elementX.value
-  rect.y1 = elementY.value
-}
-const mouseUp = ()=>{
+const mouseDown = () => {
+  rect.x = Math.round(elementX.value)
+  rect.y = Math.round(elementY.value)
   isDraw.value = true
-  rect.x2 = elementX.value
-  rect.y2 = elementY.value
 }
+const mouseUp = () => {
+  isDraw.value = false
+}
+
+const rr = computed(() => {
+  if (isDraw.value) {
+    rect.x1 = Math.round(Math.min(rect.x, elementX.value))
+    rect.x2 = Math.round(Math.max(rect.x, elementX.value))
+    rect.y1 = Math.round(Math.min(rect.y, elementY.value))
+    rect.y2 = Math.round(Math.max(rect.y, elementY.value))
+  }
+  return rect
+
+})
 
 </script>
 
 <template>
-  <div class="svg_container flex" >
-    <div class="info w-1/3 p-3">
-      <div>pos: {{ x }}, {{ y }}</div>
-      <div>pos: {{ elementX }}, {{ elementY }}</div>
+  <div class="svg_container flex">
+    <div class="info w-60 p-3">
+      <div>posXY: {{ x }}, {{ y }}</div>
+      <div>posXY el: {{ elementX }}, {{ elementY }}</div>
       <div>isOutside: {{ isOutside }}</div>
-      <div>rect: <pre>{{ rect }}</pre></div>
+      <div>rect:
+        <pre>{{ rr }}</pre>
+      </div>
     </div>
     <svg class="draw_container grow h-full" ref="target" @mousedown="mouseDown" @mouseup="mouseUp">
-      <!-- <SvgRect :x="rect.x1" :y="rect.y1" :rwidth="rect.x2-rect.x1" :rheight="rect.y2-rect.y1"/> -->
-      <SvgRect :rect="rect"/>
+      <SvgRect :x1="rect.x1" :y1="rect.y1" :x2="rect.x2" :y2="rect.y2" />
+      <!-- <SvgRect :rect="rect"/> -->
     </svg>
   </div>
 </template>
 
 <style lang="scss">
+.svg_container {
+  border: 1px solid #5a5a5a;
+  width: 800px;
+  height: 350px;
+  user-select: none;
 
-  .svg_container{
-    border: 1px solid #5a5a5a;
-    width: 800px;
-    height: 350px;
-    user-select: none;
-    .info{
-      border-right: 1px solid #5a5a5a;
-    }
+  .info {
+    border-right: 1px solid #5a5a5a;
   }
-
+}
 </style>
--- src/components/SvgRect.vue
+++ src/components/SvgRect.vue
@@ -2,25 +2,21 @@
 
 import RRect from '../models/RRect';
 
-const props =  defineProps<RRect>()
+const props = defineProps<{
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+}>()
 
 
 
 </script>
 
 <template>
-  <rect class="" ref="" 
-    :x="props.x1" 
-    :y="props.y1" 
-    :width="props.x2 - props.x1" 
-    :height="props.y2 - props.y1" 
-    style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);"
-  >    
+  <rect class="" ref="" :x="props.x1" :y="props.y1" :width="props.x2 - props.x1" :height="props.y2 - props.y1"
+    style="fill: rgb(216, 216, 216); stroke: rgb(0, 0, 0);">
   </rect>
 </template>
 
-<style lang="scss">
-
-
-
-</style>
+<style lang="scss"></style>
